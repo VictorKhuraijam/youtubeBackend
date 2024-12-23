eaab4d05ed8a2ae169c1738638dbbcd6b6f5f50e
Victorkhuraijam <khuraijamvictor@gmail.com>
2024-12-23 18:19:28 +0530

    added more router controller and changes the command set to unset in the logout controller to remove the refresh token field as undefined is ignored in mongoDb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,12 @@ import {ApiError} from '../utils/ApiError.js'
 import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {ApiResponse} from '../utils/ApiResponse.js'
+import jwt from 'jsonwebtoken'
 
-
+const options = {
+  httpOnly: true,
+  secure: true
+}
 
 const generateAccessTokenAndRefreshToken = async (userId) => {
   try {
@@ -13,7 +17,16 @@ const generateAccessTokenAndRefreshToken = async (userId) => {
     const refreshToken = user.generateRefreshToken();
 
     user.refreshToken = refreshToken
-    await user.save({validateBeforeSave: false})
+    await user.save({validateBeforeSave: false})//skips schema validation
+    /*
+    Partial Updates:
+       If you're updating a single field (e.g., refreshToken) and don't want the entire document to be validated.
+    Performance Optimization:
+       If you trust the data integrity and want to avoid the overhead of schema validation for a small, controlled update.
+    Avoiding Unintended Validation Errors:
+       If some fields are temporarily invalid but irrelevant to the current operation, skipping validation ensures the document can be saved.
+
+    */
 
     return {
       accessToken,
@@ -186,15 +199,10 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
-  const options = {
-    httpOnly: true,
-    secure: true
-  }
-
   return res
   .status(200)
-  .cookie("AccessToken", accessToken, options) //can be read but only accessable from server
-  .cookie("RefreshToken", refreshToken, options)
+  .cookie("accessToken", accessToken, options) //can be read but only accessable from server
+  .cookie("refreshToken", refreshToken, options)
   .json(
     new ApiResponse(
       200,
@@ -213,19 +221,14 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined
-      }
+      $unset: { refreshToken: "" }
     },
       {
-        new: true
+        new: true // Ensures the updated user document is returned
       }
   )
 
-  const options = {
-    httpOnly: true,
-    secure: true
-  }
+
 
   return res
   .status(200)
@@ -234,9 +237,55 @@ const logoutUser = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler(async(req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401, "Unauthorized request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id)
+
+    if(!user){
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if(incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401, "Refresh Token is expired or used")
+    }
+
+    const {accessToken, newRefreshToken} = await generateAccessTokenAndRefreshToken(user._id)
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          accessToken,
+          refreshToken : newRefreshToken
+        },
+        "Access token refreshed"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+
+})
+
 
 export {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  refreshAccessToken
 }
--- src/middlewares/auth..middleware.js
+++ src/middlewares/auth..middleware.js
@@ -1,18 +1,25 @@
 import jwt from "jsonwebtoken";
-import { ApiError } from "../utils/ApiError";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 
 
 
-export const verifyJWT = asyncHandler(async (req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _ , next) => {
   const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+  // console.log(token)
 
   if(!token){
     throw new ApiError(401, "Unauthorized request")
   }
 
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expired access token");
+  }
+  //The jwt.verify function is synchronous and will throw an error if the token is invalid, expired, or cannot be verified for any other reason. Errors thrown synchronously won't be caught by the asyncHandler because asyncHandler only catches errors returned as rejected promises.
 
   const user = await User.findById(decodedToken._id).select("-password -refreshToken")
 
